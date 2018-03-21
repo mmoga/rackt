cd556a2e72c8450b076595fceae70667efec52b6
Matthew Mogavero <matt.moga@gmail.com>
2018-03-20 21:01:28 -0400

    attempted a Google map. Not doing so hot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  // constructor(){
+  //   super();
+  //   this.initMap()
+  // }
+  // initMap() {
+  //   var uluru = {lat: -25.363, lng: 131.044};
+  //   var map = new google.maps.Map(document.getElementById('map'), {
+  //     zoom: 4,
+  //     center: uluru
+  //   });
+  //   var marker = new google.maps.Marker({
+  //     position: uluru,
+  //     map: map
+  //   });
+  // }
+  // <script
+  //   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFgktTI962eeNrPR-ugydbOJoEWxg8EOI&callback=initMap">
+  //   </script>
   render() {
     return (
       <div className="App">
-        
+        Needs a filter button and a current location button<br />
+        Add a map as well
+        <h3>Google Maps Demo</h3>
+        <div id="map" />
       </div>
     );
   }

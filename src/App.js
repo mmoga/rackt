be8e938e8bec4ba1fb60278e003eab782e7359f5
Matthew Mogavero <matt.moga@gmail.com>
2018-03-21 20:43:28 -0400

    installed react-google-maps and brought in script to render map. still breaks when clicked on

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
 import React, { Component } from "react";
 import "./App.css";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+import MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel";
+
+// use this at some point to connect to my own API
+// require('dotenv').config();
+// const { GOOGLE_MAPS_API } = process.env;
+
+const Map = withScriptjs(
+  withGoogleMap(props => {
+    return (
+      <GoogleMap
+        {...props}
+        defaultZoom={8}
+        defaultCenter={{ lat: 29.651634, lng: -82.324829 }}
+      >
+        {props.children}
+      </GoogleMap>
+    );
+  })
+);
 
 class App extends Component {
-  // constructor(){
-  //   super();
-  //   this.initMap()
-  // }
-  // initMap() {
-  //   var uluru = {lat: -25.363, lng: 131.044};
-  //   var map = new google.maps.Map(document.getElementById('map'), {
-  //     zoom: 4,
-  //     center: uluru
-  //   });
-  //   var marker = new google.maps.Marker({
-  //     position: uluru,
-  //     map: map
-  //   });
-  // }
-  // <script
-  //   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFgktTI962eeNrPR-ugydbOJoEWxg8EOI&callback=initMap">
-  //   </script>
+  constructor() {
+    super();
+    this.state = {
+      markers: []
+    };
+  }
+  addMarker(e) {
+    const lat = e.latLng.lat();
+    const lng = e.latLng.lng();
+    this.setState({
+      markers: [...this.state.markers, { lat, lng }]
+    });
+  }
   render() {
+    const props = {
+      googleMapURL:
+        "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+      loadingElement: <div style={{ height: `100%` }} />,
+      containerElement: <div style={{ height: `100vh` }} />,
+      mapElement: <div style={{ height: `100%` }} />
+    };
+    const pins = this.state.markers.map((marker, index) => (
+      <MarkerWithLabel key={index} position={marker} >i</MarkerWithLabel>
+    ));
     return (
-      <div className="App">
-        Needs a filter button and a current location button<br />
-        Add a map as well
-        <h3>Google Maps Demo</h3>
-        <div id="map" />
+      <div>
+        <Map {...props} onClick={e => this.addMarker(e)}>
+          {pins}
+        </Map>
       </div>
     );
   }
